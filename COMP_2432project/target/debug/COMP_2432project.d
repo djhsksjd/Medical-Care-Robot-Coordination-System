8b04086d3a7f14613eb47a8be164326f0dd7c5c3
Henry <lee1995127@qq.com>
2026-03-06 19:07:33 +0800

    Test the commit file

---
+++ COMP_2432project/target/debug/COMP_2432project.d
@@ -0,0 +1,1 @@
+/mnt/f/桌面/COMP2432_real_pro/COMP_2432project/target/debug/COMP_2432project: /mnt/f/桌面/COMP2432_real_pro/COMP_2432project/src/main.rs
---
+++ COMP_2432project/target/debug/deps/COMP_2432project-744c1b3a486348f7.d
@@ -0,0 +1,5 @@
+/mnt/f/桌面/COMP2432_real_pro/COMP_2432project/target/debug/deps/COMP_2432project-744c1b3a486348f7.d: src/main.rs
+
+/mnt/f/桌面/COMP2432_real_pro/COMP_2432project/target/debug/deps/COMP_2432project-744c1b3a486348f7: src/main.rs
+
+src/main.rs:
